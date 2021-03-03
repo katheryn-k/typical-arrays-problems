1027e790e718a47569b1801de5a17f9b5a16dd4f
katheryn-k <kirichenko.ekaterynaa@yandex.ru>
2021-03-03 13:41:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array && array.length > 0 ? Number(Math.min(...array)) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array && array.length > 0 ? Number(Math.max(...array)) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array && array.length > 0 ? array.reduce((a, b) => a + b, 0) / array.length : 0;
 }
